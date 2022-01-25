9f37a40935ecb69bebc1c57641d659e4aaa7bfc5
Joseba Martos <1229008-josebamartos@users.noreply.gitlab.com>
2022-01-25 12:08:32 +0000

    Replaced SSH+Git based repo clonation with HTTPS based.

--- installer/remote-install.sh
+++ installer/remote-install.sh
@@ -5,7 +5,7 @@ echo -e "\n[INFO] Cloning otzarri-hacking"
 cur_dir=$(pwd)
 tmp_dir=$(mktemp -d)
 cd "${tmp_dir}"
-git clone git@gitlab.com:josebamartos/otzarri-hacking.git
+git clone https://gitlab.com/josebamartos/otzarri-hacking.git
 "${tmp_dir}"/otzarri-hacking/installer/install.sh "${action}"
 cd "${cur_dir}"
 rm -rf "${tmp_dir}"
